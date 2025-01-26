00cc270d86c9bb643bacdde4c6dbc342747bb920
Szymon Soltysiak <szymon.soltysiak@gmail.com>
2025-01-26 02:12:50 +0100

    Fix layout

--- src/_mock/_data.ts
+++ src/_mock/_data.ts
@@ -98,26 +98,6 @@ export const _products = [...Array(24)].map((_, index) => {
 
 // ----------------------------------------------------------------------
 
-export const _langs = [
-  {
-    value: 'en',
-    label: 'English',
-    icon: '/assets/icons/flags/ic-flag-en.svg',
-  },
-  {
-    value: 'de',
-    label: 'German',
-    icon: '/assets/icons/flags/ic-flag-de.svg',
-  },
-  {
-    value: 'fr',
-    label: 'French',
-    icon: '/assets/icons/flags/ic-flag-fr.svg',
-  },
-];
-
-// ----------------------------------------------------------------------
-
 export const _timeline = [...Array(5)].map((_, index) => ({
   id: _id(index),
   title: [
@@ -137,53 +117,3 @@ export const _tasks = [...Array(5)].map((_, index) => ({
   id: _id(index),
   name: _taskNames(index),
 }));
-
-// ----------------------------------------------------------------------
-
-export const _notifications = [
-  {
-    id: _id(1),
-    title: 'Your order is placed',
-    description: 'waiting for shipping',
-    avatarUrl: null,
-    type: 'order-placed',
-    postedAt: _times(1),
-    isUnRead: true,
-  },
-  {
-    id: _id(2),
-    title: _fullName(2),
-    description: 'answered to your comment on the Minimal',
-    avatarUrl: '/assets/images/avatar/avatar-2.webp',
-    type: 'friend-interactive',
-    postedAt: _times(2),
-    isUnRead: true,
-  },
-  {
-    id: _id(3),
-    title: 'You have new message',
-    description: '5 unread messages',
-    avatarUrl: null,
-    type: 'chat-message',
-    postedAt: _times(3),
-    isUnRead: false,
-  },
-  {
-    id: _id(4),
-    title: 'You have new mail',
-    description: 'sent from Guido Padberg',
-    avatarUrl: null,
-    type: 'mail',
-    postedAt: _times(4),
-    isUnRead: false,
-  },
-  {
-    id: _id(5),
-    title: 'Delivery processing',
-    description: 'Your order is being shipped',
-    avatarUrl: null,
-    type: 'order-shipped',
-    postedAt: _times(5),
-    isUnRead: false,
-  },
-];
--- src/layouts/dashboard/layout.tsx
+++ src/layouts/dashboard/layout.tsx
@@ -2,25 +2,16 @@ import type { Theme, SxProps, Breakpoint } from '@mui/material/styles';
 
 import { useState } from 'react';
 
-import Box from '@mui/material/Box';
 import Alert from '@mui/material/Alert';
 import { useTheme } from '@mui/material/styles';
-
-import { _langs, _notifications } from 'src/_mock';
-
-import { Iconify } from 'src/components/iconify';
 
 import { Main } from './main';
 import { layoutClasses } from '../classes';
 import { NavMobile, NavDesktop } from './nav';
 import { navData } from '../config-nav-dashboard';
-import { Searchbar } from '../components/searchbar';
 import { MenuButton } from '../components/menu-button';
 import { LayoutSection } from '../core/layout-section';
 import { HeaderSection } from '../core/header-section';
-import { AccountPopover } from '../components/account-popover';
-import { LanguagePopover } from '../components/language-popover';
-import { NotificationsPopover } from '../components/notifications-popover';
 
 // ----------------------------------------------------------------------
 
@@ -76,32 +67,7 @@ export function DashboardLayout({ sx, children, header }: DashboardLayoutProps) 
                 />
               </>
             ),
-            rightArea: (
-              <Box gap={1} display="flex" alignItems="center">
-                <Searchbar />
-                <LanguagePopover data={_langs} />
-                <NotificationsPopover data={_notifications} />
-                <AccountPopover
-                  data={[
-                    {
-                      label: 'Home',
-                      href: '/',
-                      icon: <Iconify width={22} icon="solar:home-angle-bold-duotone" />,
-                    },
-                    {
-                      label: 'Profile',
-                      href: '#',
-                      icon: <Iconify width={22} icon="solar:shield-keyhole-bold-duotone" />,
-                    },
-                    {
-                      label: 'Settings',
-                      href: '#',
-                      icon: <Iconify width={22} icon="solar:settings-bold-duotone" />,
-                    },
-                  ]}
-                />
-              </Box>
-            ),
+            rightArea: null,
           }}
         />
       }
